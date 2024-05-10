b24a4ea2cfd9129c7e6836c737038ee5d5b9966f
JoD34 <joseph.descarreaux@outlook.com>
2024-05-10 17:15:58 -0400

    Feat: Documentation for pre-indexation

--- RNASeq/index.sh
+++ RNASeq/index.sh
@@ -3,8 +3,21 @@
 #SBATCH --account=def-chantalg
 #SBATCH --mem=35G
 
-# Charger les modules importants pour le fonctionnement de Kallisto
+
+<<index
+ The present code is to indexed the transcriptome for further use in the quantification process (kallisto)
+ Before head, you must fetch to correct file corresponding to the transcriptome.
+ To do so, you could run the following command line:
+
+            wget https://ftp.ensembl.org/pub/release-111/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz
+
+ Visit the ENSEMBL website for other files (or any other website)
+index
+
+
+# Load modules for dependencies of kallisto and kallisto itself
 module load StdEnv/2020 gcc/9.3.0 intel/2020.1.217 kallisto/0.46.1
 
-# Indexation du transcriptome de référence
+
+# Indexation of the reference transcriptome
 kallisto index --index=Homo_sapiens.GRCh38.cdna.all.index Homo_sapiens.GRCh38.cdna.all.fa.gz 1>>log.txt 2>>err.txt
